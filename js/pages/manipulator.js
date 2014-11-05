a54a05f48f8ea9fb136264756c795ada6f0be2d7
adwisatya <a.dwisaty4@yahoo.com>
2014-11-05 22:05:32 +0700

    Penambahan register

--- js/pages/manipulator.js
+++ js/pages/manipulator.js
@@ -6,6 +6,10 @@ function clickHome(){
 	document.getElementById("editable-starter-template").innerHTML = "<h1>Silahkan Login</h1><section class=\"login-form-wrap\"><h1>MATERIALIST</h1><form class=\"login-form\" action=\"POST\" action=\"#\"><label>	<input type=\"email\" name=\"email\" required placeholder=\"Email\"></label><label>	<input type=\"password\" name=\"password\" required placeholder=\"Password\"></label><input type=\"submit\" value=\"Login\">	<label>	<br>OR<input type=\"submit\" value=\"Log in with Facebook\"></form>	</section>";
 }
 
+function clickRegister(){
+	document.getElementById("editable-starter-template").innerHTML = "<h1>Silahkan Mendaftar</h1><section class=\"login-form-wrap\"><h1>CALON MATERIALIST</h1><form class=\"login-form\" action=\"POST\" action=\"#\"><label>	<input type=\"email\" name=\"nama\" required placeholder=\"Nama\"></label><label>	<input type=\"email\" name=\"email\" required placeholder=\"Email\"></label><label>	<input type=\"password\" name=\"password\" required placeholder=\"Password\"></label><input type=\"submit\" value=\"Daftar\"></form>	</section>";
+}
+
 function clickContact(){
 	document.getElementById("editable-starter-template").innerHTML= "<h1>HUBUNGI KAMI</h1><h3>admin@phom.material.itb.ac.id</h3><iframe src=\"https://www.google.com/maps/embed?pb=!1m27!1m12!1m3!1d3961.0040568389354!2d107.60909199853852!3d-6.890116254488398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m12!1i0!3e6!4m5!1s0x2e68e65767c9b183%3A0x2478e3dcdce37961!2sInstitut+Teknologi+Bandung%2C+Jl.+Tamansari+64%2C+Jawa+Barat+40116%2C+Indonesia!3m2!1d-6.89148!2d107.610659!4m3!3m2!1d-6.889632!2d107.6094037!5e0!3m2!1sen!2ssg!4v1415038291896\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\"></iframe>";
 }
